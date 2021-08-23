e86cc8997083c64050a13b124317ed42f63db987
Alvin(Xinyao) Sun <lucklyric@gmail.com>
2021-08-23 12:54:07 -0700

    Revert "feat[zsh]: add auto-complete"
    This reverts commit 6cb3ba131969f263fafc36cf510868d0ce538eda.

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,6 @@
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/marlonrichert/zsh-autocomplete $HOME/.oh-my-zsh/custom/plugins/zsh-autocomplete
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 ln -srf ./ohmyzsh/lib/common_config.zsh $HOME/.oh-my-zsh/lib/common_config.zsh
 
