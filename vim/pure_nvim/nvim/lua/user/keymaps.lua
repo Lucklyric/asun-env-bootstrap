21eae7a8bfc1eae131d2d34e3e2e144b1558c55a
Alvin(Xinyao) Sun <lucklyric@gmail.com>
2021-12-31 13:55:00 -0800

    feat(vim): Improved clipboard

--- vim/pure_nvim/nvim/lua/user/keymaps.lua
+++ vim/pure_nvim/nvim/lua/user/keymaps.lua
@@ -60,6 +60,10 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
+-- Enhance clipboard
+keymap("", ",<C-y>", '"+y', opts)
+keymap("", ",<C-p>", '"+p', opts)
+
 -- Terminal --
 -- Better terminal navigation
 -- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
