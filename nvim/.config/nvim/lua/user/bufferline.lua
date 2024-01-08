5b0f56dc857c66a35aded637dbaee8e721111dd3
asun <lucklyric@gmail.com>
2024-01-08 15:22:54 -0700

    feat(vim): update plugins

--- nvim/.config/nvim/lua/user/bufferline.lua
+++ nvim/.config/nvim/lua/user/bufferline.lua
@@ -3,7 +3,7 @@ if not status_ok then
   return
 end
 vim.cmd [[
-  nnoremap <silent> gb :BufferLinePick<CR>
+  " nnoremap <silent> gb :BufferLinePick<CR>
   " These commands will navigate through buffers in order regardless of which mode you are using
   " e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
   nnoremap <silent>b[ :BufferLineCycleNext<CR>
@@ -12,17 +12,17 @@ vim.cmd [[
 
 bufferline.setup {
   options = {
-    numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-    close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+    numbers = "none",                    -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+    close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
     right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-    left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-    middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+    left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
+    middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
     -- NOTE: this plugin is designed with this icon in mind,
     -- and so changing this is NOT recommended, this is intended
     -- as an escape hatch for people who cannot bear it for whatever reason
     indicator = {
       style = "icon",
-      icon =  "▎",
+      icon = "▎",
     },
     buffer_close_icon = "",
     -- buffer_close_icon = '',
@@ -44,7 +44,7 @@ bufferline.setup {
     max_name_length = 30,
     max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
     tab_size = 21,
-    diagnostics = false, -- | "nvim_lsp" | "coc",
+    diagnostics = false,    -- | "nvim_lsp" | "coc",
     diagnostics_update_in_insert = false,
     -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
     --   return "("..count..")"
